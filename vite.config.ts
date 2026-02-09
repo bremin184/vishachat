ea40886457ad3efd6c5d3ebc36a864b2b9b872c1
bremin184 <breminkiptoo50@gmail.com>
2026-02-09 18:49:39 +0300

    solve device connection problem

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,19 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "::", // allows external access
     port: 8080,
     hmr: {
       overlay: false,
+      protocol: "wss",
+      host: " https://16d4-196-96-68-63.ngrok-free.app",
+      port: 8080,
     },
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '.ngrok-free.app', // allow ngrok domains
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
